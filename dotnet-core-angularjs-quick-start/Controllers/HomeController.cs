9f5a57045a22e532c6885c340a2b0f752853f767
yanglei <yanglei@tidebuy.net>
2017-04-19 23:18:54 +0800

    初创

---
+++ dotnet-core-angularjs-quick-start/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShadowsocksMessage.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
